adc4c64fbf23a19f3f76174c670777e97929c2bc
Aidan-97 <82592337+Aidan-97@users.noreply.github.com>
2025-10-05 17:36:50 +0100

    Update App.js

--- Ratchet & Clank Weapon Game/ReactJS Version/src/App.js
+++ Ratchet & Clank Weapon Game/ReactJS Version/src/App.js
@@ -13,6 +13,8 @@ import ImageListDisplay from './components/ImageListDisplay';
 import { weapons, weaponImages, weaponStats } from './components/WeaponArrays';
 import RefreshIcon from '@mui/icons-material/Refresh'
 import HelpIcon from '@mui/icons-material/Help'
+import headerIcon from "./images/icon-header.png"
+import "./fonts/simiandisplay-orangutan.otf"
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction='up' ref={ref} {...props} />;
@@ -55,41 +57,57 @@ class App extends Component {
     })
   }
 
+  // method for dealing with submitted guess: checks/changes listed in comments below
   handleSubmit(currentGuess){
+    // increment no. of guesses by 1
     this.setState({ guesses: this.state.guesses + 1 })
 
+    // add newly guessed weapon to weaponsGuessed array to track all guesses & prevent same weapon being guessed again
     const newWeaponsGuessed = this.state.weaponsGuessed.slice()
     newWeaponsGuessed[this.state.guesses] = currentGuess
     this.setState({ weaponsGuessed: newWeaponsGuessed })
 
+    // flip panels for corresponding guess no.
     const newGuessesFlipped = this.state.guessesFlipped.slice()
     newGuessesFlipped[this.state.guesses] = true
     this.setState({ guessesFlipped: newGuessesFlipped })
 
+    // reset guess value
     this.setState({ guess: "" })
 
+    // check if guessed weapon matches target weapon
     this.checkWin(currentGuess, this.state.targetWeapon)
   }
 
   checkWin(guess, target) {
     if (guess === target){
-      setTimeout( () => {this.setState({ hasWon: true })}, 800)
+      setTimeout( () => {this.setState({ hasWon: true })}, 800) // delay added to allow full panel flipping animation to play before win/lose pop-up message
     } else if (guess !== target && this.state.guesses === 5){
       this.setState({ hasLost: true })
     }
   }
 
+  // basic reset after game won or lost
   refresh(){
     this.setState({
-      guess: "",
-      guesses: 0,
-      weaponsGuessed: ["", "", "", "", "", ""],
-      guessesFlipped: [false, false, false, false, false, false],
-      hasWon: false,
-      hasLost: false
-    })
+        guess: "",
+        guesses: 0,
+        weaponsGuessed: ["", "", "", "", "", ""],
+        guessesFlipped: [false, false, false, false, false, false],
+        hasWon: false,
+        hasLost: false
+      })
 
     this.getTargetWeapon()
+  }
+
+  // for when refresh icon in header is clicked, user prompted to confirm they want to restart first before refreshing
+  earlyRefresh(){
+    const isConfirmed = window.confirm("Are you sure you want to restart?")
+
+    if (isConfirmed) {
+      this.refresh();
+    }
   }
 
   render() {
@@ -99,18 +117,21 @@ class App extends Component {
           <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static" style={{backgroundColor: "rgba(245, 242, 0, 0.9)"}}>
               <Toolbar>
+
+                {/** 'isBrowser' from 'react-device-detect' npm package used here to alter display of toolbar on browser vs on mobile, used on multiple elements in this program */}
+
                 { isBrowser ? 
                 <>
-                  <RefreshIcon onClick={() => this.refresh()} style={{marginLeft: "5%"}} />
+                  <RefreshIcon onClick={() => this.earlyRefresh()} style={{marginLeft: "5%"}} />
 
-                  <h3 style={{display: "flex", alignItems: "center", justifyContent: "center", width: "80%", marginLeft: "10%", marginRight: "10%", color: "white", fontSize: "90%"}}><Font family="Press Start 2P">Ratchet & Clank Weapon Game</Font></h3>
+                  <h3 style={{display: "flex", alignItems: "center", justifyContent: "center", width: "80%", marginLeft: "10%", marginRight: "10%", color: "white", fontSize: "90%"}}><Font family="Press Start 2P">Which</Font><img style={{width: "40px", marginRight: "5px"}} alt="" src={headerIcon}/><Font family="Press Start 2P">Weapon?</Font></h3>
 
                   <HelpIcon onClick={() => this.setState({showHelp: true})} style={{marginRight: "5%"}}/>
                 </> :
                 <>
-                  <RefreshIcon onClick={() => this.refresh()} />
+                  <RefreshIcon onClick={() => this.earlyRefresh()} />
 
-                  <h3 style={{display: "flex", alignItems: "center", justifyContent: "center", width: "85%", marginLeft: "7.5%", marginRight: "7.5%", color: "white", fontSize: "50%"}}><Font family="Press Start 2P">Ratchet & Clank Weapon Game</Font></h3>
+                  <h3 style={{display: "flex", alignItems: "center", justifyContent: "center", width: "85%", marginLeft: "7.5%", marginRight: "7.5%", color: "white", fontSize: "60%"}}><Font family="Press Start 2P">Which</Font><img style={{width: "40px", marginRight: "5px"}} alt="" src={headerIcon}/><Font family="Press Start 2P">Weapon?</Font></h3>
 
                   <HelpIcon onClick={() => this.setState({showHelp: true})}/>
                 </>
@@ -132,7 +153,7 @@ class App extends Component {
                         <li>Try to guess correctly within 6 attempts, the correct weapon will be shown in the pop-up message at the end of the game</li>
                         <h4>Reminders</h4>
                         <li>For deployable turret-type weapons (e.g. the Miniturret Glove) range, and rate of fire refer to the turret, not the glove</li><br/>
-                        <li>Since cost refers ONLY to bolts, weapons earned by finding schematics (e.g. RYNO IV) or via battle arena challenges (e.g. Constructo Shotgun) will have a cost of 0, as will weapons that the player starts each game with</li>
+                        <li>Since cost refers ONLY to bolts, weapons earned by finding schematics (e.g. RYNO V) or via battle arena challenges (e.g. Constructo Shotgun) will have a cost of 0, as will weapons that the player starts each game with</li>
                       </DialogContentText>
                     </DialogContent>
                     <DialogActions>
@@ -155,7 +176,7 @@ class App extends Component {
                 disablePortal
                 id="guess"
                 options={weapons.slice(1)}
-                sx={{width: 250, backgroundColor: "white", "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {borderColor: "rgb(255, 255, 255, 0.1)"}}}
+                sx={{width: 250, backgroundColor: "white", border: "-1.5px solid", borderColor: "white", borderRadius: "4px", "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {borderColor: "rgb(255, 255, 255, 0.1)"}}}
                 onChange={(e) => this.setState({guess: e.target.textContent})}
                 onInputChange={(e) => this.handleChange(e)}
                 renderInput={(params) => <TextField {...params} label="" />}
@@ -164,16 +185,18 @@ class App extends Component {
                 disablePortal
                 id="guess"
                 options={weapons.slice(1)}
-                sx={{width: "62.5%", backgroundColor: "white", "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {borderColor: "rgb(255, 255, 255, 0.1)"}}}
+                sx={{width: "62.5%", backgroundColor: "white", border: "-1.5px solid", borderColor: "white", borderRadius: "4px", "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {borderColor: "rgb(255, 255, 255, 0.1)"}}}
                 onChange={(e) => this.setState({guess: e.target.textContent})}
                 onInputChange={(e) => this.handleChange(e)}
                 renderInput={(params) => <TextField {...params} label="" />}
               /> 
             }</>
 
+            {/** Submit button only activated if properly entered, newly guessed weapon is in the autocomplete input box */}
+
             <>{ this.state.guess !== "" && weapons.indexOf(this.state.guess) !== -1 && this.state.weaponsGuessed.indexOf(this.state.guess) === -1 ?
-              <Button variant="contained" style={{height: 56.1, marginLeft: "-0.5%", backgroundColor: "rgb(245, 242, 0)", fontWeight: "bolder", color: "black"}} onClick={() => this.handleSubmit(this.state.guess)}>Submit</Button> :
-              <Button variant="contained" style={{height: 56.1, backgroundColor: "lightgray", marginLeft: "-0.5%"}} disabled>Submit</Button>
+              <Button variant="contained" style={{height: 56, marginLeft: "-5px", backgroundColor: "rgb(245, 242, 0)", fontWeight: "bolder", color: "black"}} onClick={() => this.handleSubmit(this.state.guess)}>Submit</Button> :
+              <Button variant="contained" style={{height: 56, backgroundColor: "lightgray", marginLeft: "-5px"}} disabled>Submit</Button>
             }</>
 
           </Box>
@@ -226,30 +249,34 @@ class App extends Component {
               </Dialog>
             </>
           
+          {/** Similar to use of 'isBrowser', elements in '<BrowserView> tags are only rendered on browser, used here to create row of labels for grid of guesses as shown on browser */}
+
           <BrowserView>
             <Box style={{display: "flex", alignItems: "center", justifyContent: "center", marginTop: "2.5%", marginBottom: "1%"}}>
               <Grid style={{display: "flex", alignItems: "center", justifyContent: "center"}} container spacing={2}>
                 <Grid item>
-                  <div style={{width: 100, textAlign: "center", color: "white"}}><Font family="Helvetica">Console</Font></div>
+                  <div style={{width: 100, textAlign: "center", color: "white", fontFamily: "SD", fontSize: "115%"}}>Console</div>
                 </Grid>
                 <Grid item>
-                  <div style={{width: 100, textAlign: "center", color: "white"}}><Font family="Helvetica">Game</Font></div>
+                  <div style={{width: 100, textAlign: "center", color: "white", fontFamily: "SD", fontSize: "115%"}}>Game</div>
                 </Grid>
                 <Grid item>
-                  <div style={{width: 100, textAlign: "center", color: "white"}}><Font family="Helvetica">Planet/Level</Font></div>
+                  <div style={{width: 100, textAlign: "center", color: "white", fontFamily: "SD", fontSize: "115%"}}>Planet/Level</div>
                 </Grid>
                 <Grid item>
-                  <div style={{width: 100, textAlign: "center", color: "white"}}><Font family="Helvetica">Range</Font></div>
+                  <div style={{width: 100, textAlign: "center", color: "white", fontFamily: "SD", fontSize: "115%"}}>Range</div>
                 </Grid>
                 <Grid item>
-                  <div style={{width: 100, textAlign: "center", color: "white"}}><Font family="Helvetica">Rate of Fire</Font></div>
+                  <div style={{width: 100, textAlign: "center", color: "white", fontFamily: "SD", fontSize: "115%"}}>Rate of Fire</div>
                 </Grid>
                 <Grid item>
-                  <div style={{width: 100, textAlign: "center", color: "white"}}><Font family="Helvetica">Cost (Bolts)</Font></div>
+                  <div style={{width: 100, textAlign: "center", color: "white", fontFamily: "SD", fontSize: "115%"}}>Cost (Bolts)</div>
                 </Grid>
               </Grid>
             </Box>
           </BrowserView>
+
+          {/** 'isBrowser used again, this time to create 6x6 grid of guess panels on browser vs 6 separate 3x2 grids of panels on mobile */}
 
           <>{ isBrowser ?
 
@@ -271,7 +298,7 @@ class App extends Component {
               <Grid container justifyContent="center" item xs={12}>
                 {[1, 2, 3, 4, 5, 6].map((value) => (
                   <div style={{marginBottom: "7.5%"}}>
-                    <h4 style={{marginBottom: "2.5%", color: "white", display: "flex", alignItems: "center", justifyContent: "center"}}>Guess {value}</h4>
+                    <h4 style={{marginBottom: "2.5%", color: "white", display: "flex", alignItems: "center", justifyContent: "center", fontFamily: "SD"}}>Guess {value}</h4>
                     <Guess guessStats={weaponMap.get(this.state.weaponsGuessed[value-1])} targetStats={weaponMap.get(this.state.targetWeapon)} isFlipped={this.state.guessesFlipped[value-1]}/>
                   </div>
                 ))}
@@ -286,6 +313,8 @@ class App extends Component {
     );
   }
 }
+
+// weapons mapped to their stats & images below, with values for weapon names, stats & images all imported from 'weaponArrays' file
 
 const weaponMap = new Map();
 const weaponImageMap = new Map();
